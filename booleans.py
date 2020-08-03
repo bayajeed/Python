e9493ed20a49879bcc19468357b8cffc99a2e9d4
Bayajeed <bayajeedhossain@gmail.com>
2020-08-03 20:07:54 +0600

    first commit

---
+++ booleans.py
@@ -0,0 +1,27 @@
+A = True
+B = True
+C = False
+D = False
+
+#00=0
+#10=0
+#01=0
+#11=1
+print("And Gate: " ,A and B and C) # And Gate
+
+#00=0
+#10=1
+#01=1
+#11=1
+print("Or Gate: " ,A or C) # Or Gate
+
+"""  same digit are False
+Defferant type are True
+00=0
+10=1
+01=1
+11=0                         
+"""
+print("X-or" ,A ^ D ) 
+
+
---
+++ hello.py
@@ -0,0 +1,14 @@
+#This is my first practice of PYTHON
+print ("hello! this is my new world ")
+""" hEA
+ALDKJF JASDJFLK
+ASDF AJSLFJS D
+FASDF JASLDFJ
+ADF;AJFLASF
+ADSFJAKLDFJ
+LJSLKDFJDSFL """
+name="Bayaljeed"
+print("Name "+name, type(name))
+age=20
+weight = 50
+print("Age",age, type(age), "\nWeight " ,weight)
